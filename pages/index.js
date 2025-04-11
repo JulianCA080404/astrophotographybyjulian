c4b15584177a870ba10d8139281c5e7fa0b1c8db
JulianCA080404 <jcaruanaabela2004@gmail.com>
2025-04-11 11:44:09 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{
+      backgroundColor: 'black',
+      color: 'white',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    }}>
+      <h1>Astrophotography By Julian</h1>
+      <p>Site is being built... coming soon.</p>
+    </div>
+  );
+}
