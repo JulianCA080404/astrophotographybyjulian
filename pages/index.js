daf52ef7aa43965e74c65ff610823d729fa25304
JulianCA080404 <jcaruanaabela2004@gmail.com>
2025-04-11 11:47:28 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,3 @@
-
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div style={{
-      backgroundColor: 'black',
-      color: 'white',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      flexDirection: 'column'
-    }}>
-      <h1>Astrophotography By Julian</h1>
-      <p>Site is being built... coming soon.</p>
-    </div>
-  );
+  return <div style={{ background: 'black', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}><h1>Astrophotography By Julian</h1></div>
 }
